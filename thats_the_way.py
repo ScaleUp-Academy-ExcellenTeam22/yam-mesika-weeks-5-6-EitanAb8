6cadaac65acb8dfa888d815657f5296a0bae3c3d
EitanAb8 <88735044+EitanAb8@users.noreply.github.com>
2022-03-28 17:00:04 +0200

    Fixed according to CR

--- thats_the_way.py
+++ thats_the_way.py
@@ -2,16 +2,19 @@ from os import listdir
 from os.path import isfile, join
 
 
-def that_the_way(path):
-    file_names = [f for f in listdir(path) if isfile(join(path,f)) and 'deep' in f]
-    return file_names
+def find_name_files(path):
+    """
+    The function returns a list of all file names starts with the word "deep" in a directory.
+    :param path: A full path to the desired directory.
+    :return: A list of the file names in the desired directory who starts with "deep".
+    """
+    return [file_name for file_name in listdir(path) if isfile(join(path, file_name)) and file_name.startswith("deep")]
 
 
 def main():
     path = r"C:\ExcellentTeam\Python_course\Week2\exercises\Notebooks\week05\images"
-    print(that_the_way(path))
+    print(find_name_files(path))
 
 
 if __name__ == '__main__':
     main()
-
