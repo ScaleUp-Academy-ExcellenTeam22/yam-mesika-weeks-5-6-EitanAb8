7798c2f55619c9bb38e5ff171b7dafe1ff80162d
EitanAb8 <88735044+EitanAb8@users.noreply.github.com>
2022-03-29 12:30:42 +0200

    Fixed according to CR

--- cup_of_join.py
+++ cup_of_join.py
@@ -1,21 +1,18 @@
-
-def join(*lists, sep="-"):
-    # This function receives lists and sep sign
-    # returns a new combined list with the sep sign between
-    # each list added.
-
-    if not len(lists):
-        return None
-    final_list = [inner for outer in lists[:len(lists)-1] for inner in outer+[sep]] + lists[len(lists)-1]
-    return final_list
+def join(*lists, separator="-") -> list:
+    """
+    The function creates a list with the given lists with a separator between them.
+    :param lists: Certain amount of lists.
+    :param separator: A string that's represent the separator.
+    :return: A list of the given parameters and a separator between each one of them.
+    """
+    return [] if not len(lists) else [inner for outer in lists[:-1] for inner in outer+[separator]]+lists[-1]
 
 
 def main():
-    
-    print(join([1, 2], [8], [9, 5, 6], sep='@'))
+    print(join([1, 2], [8], [9, 5, 6], separator="@"))
     print(join([1, 2], [8], [9, 5, 6]))
     print(join([1], [2]))
-    print(join([1]), sep='#')
+    print(join([1], separator="#"))
     print(join())
 
 
