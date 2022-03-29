0559dbb237dbb47eecec938a4f6264ededb93c02
EitanAb8 <88735044+EitanAb8@users.noreply.github.com>
2022-03-29 15:25:51 +0200

    Fixed according to CR

--- piece_of_cake.py
+++ piece_of_cake.py
@@ -1,21 +1,21 @@
+from functools import reduce
 
-def get_recipe_price(prices, optionals=None, **ingredients):
-    # This function receives:
-    # prices - dictionary of ingredients and their price for 100 grams.
-    # optionals - ingredients to ignore in the cart
-    # ingredients - grams for each ingredient we want to buy
-    # the function will calculate and return the sum of the ingredients price
 
-    final_price = 0
-    for name, price in prices.items():
-        final_price += price*(ingredients[name]/100) if name in ingredients.keys() else 0
-    return final_price
+def get_recipe_price(prices, optionals=[], **ingredients) -> float:
+    """
+    The function calculates the total price of the desired ingredients.
+    :param prices: A dictionary of ingredients and their price for 100 grams.
+    :param optionals: Ingredients to ignore in the cart.
+    :param ingredients: The Amount of grams for each ingredient the costumer wants to buy.
+    :return:
+    """
+    return reduce(lambda summary, name: summary+(prices[name]/100)*(ingredients[name])
+                  if name in ingredients.keys() and name not in optionals else summary+0, prices, 0)
 
 
 def main():
-
     print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
-    print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300))
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk', 'banana'], chocolate=300))
     print(get_recipe_price({}))
 
 
